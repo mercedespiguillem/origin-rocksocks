57bd1ec5923714639ee39521e02dfcabe069e8ef
Maria Mercedes Piguillem <mercedes.piguillem@gmail.com>
2022-01-15 23:51:21 -0300

    Desafio clase 3 y 4

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Titulo from './components/Titulo';
 import NavBar from './components/NavBar/NavBar';
-import Cartwidget from './components/CartWidget/CartWidget';
+import ItemListContainer from './components/NavBar/ItemListContainer';
 
 function App() {
   return (
     <div className="App">
         <Titulo/> 
-        {/* <Cartwidget/> */}
         <NavBar/>
+        <ItemListContainer props='Hola!'/>
     </div>
 
   );
---
+++ src/components/NavBar/ItemListContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ItemListContainer(props) {
+
+    return (
+        <div style={{backgroundColor: 'orange'}}>
+            <h3>Hola!</h3>
+        </div>
+    )
+}
+
+export default ItemListContainer
