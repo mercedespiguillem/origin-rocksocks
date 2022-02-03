e8cb9d2d02db8aea1925253830ad21af817e84b6
Maria Mercedes Piguillem <mercedes.piguillem@gmail.com>
2022-02-02 23:05:48 -0300

    Primer Pre-entrega del proyecto final

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 // import { productos } from "../../helpers/products";
 
-
-
-
-function ItemDetail({products}) {
-
-
-    return (
+function ItemDetail({ products }) {
+  return (
     <div>
-        <h3>{products.description}</h3>
-        <h6>Stock: {products.stock}</h6>
-        <h6>Precio: {products.price}</h6>
+      <div>
+        <img src={products.image} alt="" />
+      </div>
+      <h3>{products.description}</h3>
+      <h6>Stock: {products.stock}</h6>
+      <h6>Precio: {products.price}</h6>
     </div>
-    );
+  );
 }
 
 export default ItemDetail;
