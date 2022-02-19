94d6425b46f625638e6039c48d8e70a0f2c0db0f
Maria Mercedes Piguillem <mercedes.piguillem@gmail.com>
2022-02-19 19:09:00 -0300

    Desafio de la clase 13

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,11 +1,55 @@
-import React from "react";
-import { Button, ListGroup } from "react-bootstrap";
+import React, { useState } from "react";
+import { Alert, Button, ListGroup } from "react-bootstrap";
 import { useCartContext } from "../../context/CartContext";
 import { Link } from "react-router-dom";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  doc,
+  updateDoc,
+  query,
+  where,
+  documentId,
+  writeBatch,
+  getDocs,
+} from "firebase/firestore";
 
 function Cart() {
+  const [id, setId] = useState("");
+
   const { cartList, removeItem, totalPrice, totalPriceItems, clearCart } =
     useCartContext();
+
+  const makeOrder = async (e) => {
+    e.preventDefault();
+
+    let order = {};
+    // order.date = Timestamp.fromDate(new Date())
+    order.buyer = { name: "Maria", email: "maria@gmail.com", phone: 1234567 };
+    order.total = totalPrice();
+
+    order.items = cartList.map((itemInCart) => {
+      const id = itemInCart.articulo.id;
+      const title = itemInCart.articulo.name;
+      const price = itemInCart.articulo.price;
+      const amount = itemInCart.cantidad;
+
+      return {
+        id,
+        title,
+        price,
+        amount,
+      };
+    });
+  
+
+    const db = getFirestore();
+    const ordersCollection = collection(db, "orders");
+    await addDoc(ordersCollection, order).then((resp) => setId(resp.id))
+    .then((resp) => console.log(resp));
+
+  };
 
   console.log(cartList);
 
@@ -29,6 +73,7 @@ function Cart() {
                     Articulo: {prod.articulo.name} - Cantidad: {prod.cantidad} -
                     Precio: ${totalPriceItems(prod)}
                   </p>
+
                   <Button
                     variant="dark"
                     className=""
@@ -43,7 +88,11 @@ function Cart() {
           <div className="m-5">
             <h4>Total: $ {totalPrice()}</h4>
           </div>
-
+          {id !== "" && (
+            <Alert variant="success">
+              <p>El id de la orden es : {id}</p>
+            </Alert>
+          )}
           <Button
             variant="dark"
             className="m-3 p-2"
@@ -52,7 +101,7 @@ function Cart() {
           >
             Vaciar Carrito
           </Button>
-          <Button variant="dark" className="m-3 p-2">
+          <Button variant="dark" onClick={makeOrder} className="m-3 p-2">
             Finalizar compra
           </Button>
         </>
